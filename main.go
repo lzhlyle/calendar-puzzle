d824ec5b4fb223c9c898771f502a7d5a2423a623
lzhlyle <lzhlyle@msn.cn>
2021-08-09 10:37:23 +0800

    feat: Add main in project
    Change-Id: Id8b5766b9e94de0be5ef04a6de0b10c3672de1da

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"../calendar-puzzle/quick"
+)
+
+func main() {
+	// fill directions
+	for b := range quick.Blocks {
+		for d := 0; d < 4; d++ {
+			if d > 0 {
+				quick.Blocks[b][d] = quick.MoveToTopLeft(quick.Rotation(quick.Blocks[b][d-1]))
+			}
+		}
+	}
+
+	date := time.Now()
+	if len(os.Args) == 4 {
+		y, errY := strconv.Atoi(os.Args[1])
+		m, errM := strconv.Atoi(os.Args[2])
+		d, errD := strconv.Atoi(os.Args[3])
+		if errY == nil && errM == nil && errD == nil {
+			date = time.Date(y, time.Month(m), d, 0, 0, 0, 0, time.Local)
+		}
+	}
+	fmt.Println(date.Format("2006-01-02"))
+	board := quick.InitBoard(date)
+
+	// dfs
+	res, err := quick.Fill(board, quick.Blocks)
+	if err != nil {
+		fmt.Printf("FAIL, err: %v", err)
+	}
+	quick.Output(res)
+}
